304200282c1097e8c3053e6fb1e691646f898a1b
Matthew Gibson <mgib278@wgu.edu>
2024-01-30 06:18:09 -0500

    UPDATE: Added attributes to Truck and Package entities

--- entites/package.py
+++ entites/package.py
@@ -1,5 +1,5 @@
 class Package:
-  def _init_(self, package_id, address, city, state, zip_code, deadline, weight):
+  def __init__(self, package_id, address, city, state, zip_code, deadline, weight, instructions):
     self.package_id = package_id
     self.address = address
     self.city = city
@@ -7,3 +7,7 @@ class Package:
     self.zip_code = zip_code
     self.deadline = deadline
     self.weight = weight
+    self.instructions = instructions
+    self.delivered = False
+    self.departure_time = 0
+    self.delivery_time = 0
--- entites/truck.py
+++ entites/truck.py
@@ -1,7 +1,12 @@
 from HashTable import *
 
 class Truck:
-  def _init_(self):
-    self.package_list = [None] * 16
-    self.driver = False
+  def __init__(self, curr_location, speed, miles, driver, depart_time):
     self.max_packages = 16
+    self.curr_packages = 0
+    self.package_list = [None] * max_packages
+    self.driver = driver
+    self.speed = speed
+    self.miles = miles
+    self.curr_location = curr_location
+    self.depart_time = depart_time
