a4ff43847cc944f1530b4b88d63659197b1c2a20
Matthew Gibson <mgib278@wgu.edu>
2024-02-03 09:15:04 -0500

    UPDATE: Added functions for parsing addresses and the distance between addresses

--- Truck.py
+++ Truck.py
@@ -1,8 +1,7 @@
 class Truck:
-  def __init__(self, package_list, curr_location, speed, miles, driver, depart_time):
-    self.package_list = package_list
-    self.driver = driver
+  def __init__(self, curr_location, speed, miles, driver, depart_time, package_list):
+    self.curr_location = curr_location
     self.speed = speed
     self.miles = miles
-    self.curr_location = curr_location
     self.depart_time = depart_time
+    self.truck_package_list = package_list
--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 # Matthew Gibson
 
 import csv
+import datetime
 from HashTable import *
 from Package import *
 from Truck import *
 
+# Initialize package hash table
 package_list = HashTable()
 
-
+# Read in package data from csv
 with open('data\PackageData.csv') as csvpkg:
   packages = csv.reader(csvpkg, delimiter=',')
 
+  # Create package item from read in data
   for item in packages:
     package_id = int(item[0])
     address = item[1]
@@ -25,21 +28,47 @@ with open('data\PackageData.csv') as csvpkg:
 
     package = Package(package_id, address, city, state, zip_code, deadline, weight, notes)
 
+    # Add package to package hash table
     package_list.add(package_id, package)
 
-with open('data\Distances.csv') as csvdst:
+# Read in distance data from csv
+with open('data\AdjacencyMatrix.csv') as csvdst:
   distances = csv.reader(csvdst)
   distances = list(distances)
 
+# Read in address data from csv
 with open('data\Addresses.csv') as csvadd:
   addresses = csv.reader(csvadd)
   addresses = list(addresses)
 
-def findDistance(add1, add2):
-  return distances[add1][add2]
+# Method for retrieving the distance between two addresses
+def findDistance(addr1, addr2):
+  return distances[addr1][addr2]
 
-def deliverPackages(truck):
-  while len(truck.package_list) > 0:
-    
+# Method for finding the shortest edge between the current node and the given nodes
+def findMinDistance(curr_addr, pkg_list):
+  curr_addr = findAddress(curr_addr)
+  next_addr = findAddress(package_list.get(pkg_list[0]).address)
+  curr_min = findDistance(curr_addr, next_addr)
 
+  for i in range(1, len(pkg_list)-1):
+    next_addr = findAddress(package_list.get(pkg_list[i]).address)
+    dist = findDistance(curr_addr, next_addr)
+    if float(dist) < float(curr_min):
+      curr_min = dist
+  
+  return curr_min
 
+# Method for retrieving the index of an address
+def findAddress(address):
+  for addr in addresses:
+    if address in addr[2]:
+      return int(addr[0])
+
+truck1 = Truck("4001 South 700 East", 15, 0.0, True, datetime.timedelta(hours=8), [1,2,5,7,8,10,11,12,13,15,17,19,21,22,23,24])
+
+def startTruckDelivery(truck):
+  min = findMinDistance(truck.curr_location, truck.truck_package_list)
+  print(min)
+
+startTruckDelivery(truck1)
