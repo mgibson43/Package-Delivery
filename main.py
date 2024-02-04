c91d0dfa7f9258a7f9709a9de0621c55325be884
Matthew Gibson <mgib278@wgu.edu>
2024-02-04 08:19:16 -0500

    FIX: Fixed bug that would cause program to get stuck on a single address when running in loop

--- main.py
+++ main.py
@@ -41,46 +41,46 @@ with open('data\Addresses.csv') as csvadd:
   addresses = csv.reader(csvadd)
   addresses = list(addresses)
 
-# Method for retrieving the distance between two addresses
-def findDistance(addr1, addr2):
-  return distances[addr1][addr2]
-
-# Method for finding the shortest edge between the current node and the given nodes
-def findMinDistance(curr_addr, pkg_list):
-  curr_addr = findAddress(curr_addr)
-  test_addr = findAddress(package_list.get(pkg_list[0]).address)
-  curr_min = findDistance(curr_addr, test_addr)
-  next_addr = package_list.get(pkg_list[0]).address
-  curr_pkg = pkg_list[0]
-
-  for i in range(1, len(pkg_list)-1):
-    test_addr = findAddress(package_list.get(pkg_list[i]).address)
-    dist = findDistance(curr_addr, test_addr)
-    if float(dist) < float(curr_min):
-      curr_min = dist
-      next_addr = package_list.get(pkg_list[i]).address
-      curr_pkg = i
-  return [next_addr, curr_min, curr_pkg]
-
 # Method for retrieving the index of an address
 def findAddress(address):
   for addr in addresses:
     if address in addr[2]:
       return int(addr[0])
 
-truck1 = Truck("4001 South 700 East", 15, 0.0, True, datetime.timedelta(hours=8), [1,2,5,7,8,10,11,12,13,15,17,19,21,22,23,24])
+# Method for retrieving the distance between two addresses
+def findDistance(addr1, addr2):
+  return distances[addr1][addr2]
+
+def findMinDistance(curr_addr, pkg_list):
+  next_addr = package_list.get(pkg_list[0]).address
+  next_miles = findDistance(findAddress(curr_addr), findAddress(next_addr))
+  index = 0
+
+  for i in range(1, len(pkg_list) - 1):
+    test_addr = package_list.get(pkg_list[i]).address
+    test_miles = findDistance(findAddress(curr_addr), findAddress(test_addr))
+
+    if (float(next_miles) > float(test_miles)):
+      next_addr = test_addr
+      next_miles = test_miles
+      index = i
+
+  return [next_addr, next_miles, index]  
+
+truck1 = Truck("4001 South 700 East", 15, 0.0, True, datetime.timedelta(hours=8), [33,2,5,7,8,10,11,12,13,15,17,19,21,22,23,24])
 
 def startTruckDelivery(truck):
   while len(truck.truck_package_list) > 0:
-    min = findMinDistance(truck.curr_location, truck.truck_package_list)
-    print(min)
-    truck.curr_location = min[0]
-    truck.miles += float(min[1])
-    truck.truck_package_list.pop(min[2])
-    print(truck.curr_location)
+    
+    next = findMinDistance(truck.curr_location, truck.truck_package_list)
+    
+    truck.next_addr = next[0]
+    truck.miles += float(next[1])
+    truck.truck_package_list.pop(next[2])
+
+    print(truck.next_addr)
     print(truck.miles)
     print(truck.truck_package_list)
-    
- 
+
 
 startTruckDelivery(truck1)
